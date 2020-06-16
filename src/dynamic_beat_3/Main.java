15d6d20cd76543c15b6d3b1375fc78d8d877cabd
hojunthedev <ghwns5338@gmail.com>
2020-06-16 21:14:45 +0900

    인트로음악 추가(현재 에러중), mp3실행 라이브러리 추가

---
+++ src/dynamic_beat_3/Main.java
@@ -0,0 +1,14 @@
+package dynamic_beat_3;
+
+public class Main {
+
+	//선언 후 바뀌지 않게 final로 선언
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	
+	public static void main(String[] args) {
+		new DynamicBeat();
+	
+	}
+
+}
