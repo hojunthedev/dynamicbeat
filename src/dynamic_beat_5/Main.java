a5c6b93105341353912fd46c7299f6ae8af9c8ec
Hojun_Kim <ghwns5338@gmail.com>
2020-06-22 14:45:01 +0900

    start, exit 이벤트 추가, start화면전환 추가

---
+++ src/dynamic_beat_5/Main.java
@@ -0,0 +1,12 @@
+package dynamic_beat_5;
+
+public class Main {
+
+	//선언 후 바뀌지 않게 final로 선언
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	
+	public static void main(String[] args) {
+		new DynamicBeat();
+	}
+}
