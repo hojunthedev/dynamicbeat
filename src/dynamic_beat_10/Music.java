9b2909477a76a6779ec435c77b69258a219d3d8f
Hojun_Kim <ghwns5338@gmail.com>
2020-06-29 15:16:12 +0900

    게임 화면 디자인(글자 깨짐 대응)

---
+++ src/dynamic_beat_10/Music.java
@@ -0,0 +1,56 @@
+package dynamic_beat_10;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+
+import javazoom.jl.player.Player;
+
+public class Music extends Thread {
+	
+	private Player player; 	//자바줌 library
+	private boolean isLoop;	//반복인지 확인
+	private File file;
+	private FileInputStream fis;
+	private BufferedInputStream bis;
+	
+	public Music(String name, boolean isLoop) {
+		try {
+			this.isLoop = isLoop;
+			file = new File(Main.class.getResource("../music/" + name).toURI());
+			fis = new FileInputStream(file);
+			bis = new BufferedInputStream(fis);
+			player = new Player(bis);
+			System.out.println(file);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	//음악이 어떤 위치에 실행되고있는지. (0.001초 단위)
+	public int getTime() {
+		if (player == null) return 0;
+		return player.getPosition();
+	}
+	
+	public void close() {
+		isLoop = false;
+		player.close();
+		this.interrupt();	//현재 스레드 중지(곡을 실해주는 스레드)
+	}
+	
+	@Override
+	public void run() {
+		try {
+			do {
+				player.play();
+				fis = new FileInputStream(file);
+				bis = new BufferedInputStream(fis);
+				player = new Player(bis);
+			} while(isLoop);
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	
+}
